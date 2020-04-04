a1a49c861999720cc08e56c3e62560cd24c91f1e
Wellington-Yoshida <issami-yoshida@hotmail.com>
2020-04-04 13:45:47 -0300

    Criado metodos put, get, delete para atividade

--- config/router.js
+++ config/router.js
@@ -6,3 +6,9 @@ const router = express.Router();
 app.use('/api/atividade', router);
 
 router.post('/nova-atividade', atividadeController.createAtividade);
+
+router.get('/findAll-atividades', atividadeController.findAllAtividade);
+
+router.delete('/deleteBy-atividades/:id', atividadeController.deleteByAtividade);
+
+router.put('/updateBy-atividades/:id', atividadeController.updateByAtividade);
--- controlles/atividade.controller.js
+++ controlles/atividade.controller.js
@@ -4,9 +4,48 @@ const createAtividade = (req, resp, next) => {
 
     db.atividade
         .create({descricao: req.body.descricao, concluido: req.body.concluido})
-        .then(retorno => resp.send(retorno));
+        .then(retorno => resp.send(retorno))
+        .catch((err) => { throw new Error('Não foi possível cadastrar está atividade.') });
 }
 
+const findAllAtividade = (req, resp, next) => {
+
+    db.atividade
+      .findAll({
+          order: ['id']
+      }).then(resultado => resp.send(resultado))
+        .catch((err) => { throw new Error('Não foi localizado nenhuma atividade cadastrada.') })
+}
+
+const deleteByAtividade = (req, resp, next) => {
+
+    db.atividade
+      .destroy({
+          where: { id: req.params.id }
+      }).then(resultado => { resp.send('Atividade removida com sucesso.') })
+        .catch((err) => { throw new Error('Não foi possível remover está atividade.') })
+}
+
+const updateByAtividade = (req, resp, next) => {
+
+    db.atividade
+      .update({
+        descricao: req.body.descricao,
+        concluido: req.body.concluido,
+      },
+      { 
+          where: { id: req.params.id },
+          returning: true,
+          plain : true
+      })
+      .then(resultado => { resp.send(resultado) })
+      .catch((err) => { throw new Error('Não foi possível atualizar está atividade.') });
+}
+
+
 module.exports = {
-    createAtividade
+    createAtividade,
+    findAllAtividade,
+    deleteByAtividade,
+    updateByAtividade
 }
